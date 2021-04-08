296eec113e0abedd19428106ffd33d5629232bcd
ibricks <wjdgus5625@naver.com>
2021-04-08 14:13:21 +0900

    Add search directory

---
+++ routes/controller/search/search.controller.js
@@ -0,0 +1,19 @@
+import { validationResult } from "express-validator";
+
+const search = (req, res, next) => {
+    try {
+        const errors = validationResult(req);
+        
+        if (!errors.isEmpty()) {
+            return res.status(400).json(errors)
+        }
+
+        return res.json({ reqParams: req.query })
+    } catch (error) {
+        res.status(400).json(err)
+    }
+}
+
+export {
+  search
+}
