91e8752acb34e0b8e51dcb91c735e7d96a7df218
ibricks <wjdgus5625@naver.com>
2021-04-09 11:32:45 +0900

    jest test code sample 추가

--- routes/index.server.routes.js
+++ routes/index.server.routes.js
@@ -1,7 +1,9 @@
 import test from "./controller/test";
+import search from "./controller/search";
 
 const router = (app) => {
   app.use("/test", test);
+  app.use("/search", search);
 };
 
 export default router;
---
+++ test/test.js
@@ -0,0 +1,14 @@
+import request from "supertest";
+import server from "../app";
+
+describe("Test the root path", () => {
+  test("It should response the GET method", (done) => {
+    request(server)
+      .get("/search")
+      .then((res) => {
+        expect(res.statusCode).toBe(200);
+        done();
+      });
+  });
+});
+
